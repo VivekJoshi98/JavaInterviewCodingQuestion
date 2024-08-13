3d40ce12e2ee09deb087b10898e85ed9f2d72d66
Vivek joshi <vivekjoshi98.vj@gmail.com>
2024-08-13 13:07:14 +0530

    Commit changes

---
+++ src/com/Kunal/DSA/BinarySearch/FloorOfNumber.java
@@ -0,0 +1,39 @@
+package com.Kunal.DSA.BinarySearch;
+
+public class FloorOfNumber {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {2,4,6,7,9,10,13,15,18};
+		int target=1;
+		int binarySearch = floor(arr,target);
+		System.out.println(binarySearch);
+	}
+
+	//	return the index : greatest number <= target
+	private static int floor(int[] arr, int target) {
+		
+		int start=0;
+		int end=arr.length-1;
+		
+		while(start<=end)
+		{
+			int mid=(start+end)/2;
+			
+			if(target>arr[mid])
+			{
+				start=mid+1;
+			}
+			else if(target<arr[mid])
+			{
+				end=mid-1;
+			}
+			else
+			{
+				return mid;
+			}
+		}
+		return end;
+		
+	}
+}
